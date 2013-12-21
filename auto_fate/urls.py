da878779183278fb57c6b97856c17a1cd0cd2547
bsc52001 <bsc52001@gmail.com>
2013-12-21 13:57:21 -0600

    Playing with Django data models and rest api

--- auto_fate/urls.py
+++ auto_fate/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, include, url
 from rest_framework import routers
-from fate_core import views
+from fate_core import views as core_views
+from fate_character import views as character_views
 
 router = routers.DefaultRouter()
-router.register(r'games', views.GameViewSet)
-router.register(r'issues', views.IssueViewSet)
-router.register(r'faces', views.FaceViewSet)
-router.register(r'places', views.PlaceViewSet)
-router.register(r'skills', views.SkillViewSet)
+router.register(r'games', core_views.GameViewSet)
+router.register(r'issues', core_views.IssueViewSet)
+router.register(r'faces', core_views.FaceViewSet)
+router.register(r'places', core_views.PlaceViewSet)
+router.register(r'skills', core_views.SkillViewSet)
+router.register(r'characters', character_views.CharacterViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browseable API.
--- fate_core/models.py
+++ fate_core/models.py
@@ -5,6 +5,20 @@ class Game (models.Model):
     name = models.CharField(max_length=128)
     setting = models.CharField(max_length=128)
 
+    ## Dials
+    ## TODO: Should this be a class
+    number_of_aspects = models.IntegerField(default=5)
+    number_of_phases = models.IntegerField(default=3)
+    skill_cap = models.IntegerField(default=4)
+    ## TODO: Skill shape enum
+    ## TODO: Number of columns
+    refresh_rate = models.IntegerField(default=3)
+    initial_stunts = models.IntegerField(default=3)
+    ## TODO: Stress tracks
+    number_of_stress_boxes = models.IntegerField(default=2)
+    ## TODO: Consequence slots
+
+
     def __str__(self):
         return self.name
 
