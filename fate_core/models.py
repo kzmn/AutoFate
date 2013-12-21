ed5d04b4581f7eae469fe157730df2c24b8b9bbc
kzmn <kzmn@vt.edu>
2013-12-21 13:20:47 -0600

    Added some vertical whitespace

--- fate_core/models.py
+++ fate_core/models.py
@@ -1,37 +1,42 @@
 from django.db import models
 
 # Create your models here.
-class Game (models.Model):
+class Game(models.Model):
     name = models.CharField(max_length=128)
     setting = models.CharField(max_length=128)
 
     def __str__(self):
         return self.name
 
-class Aspect (models.Model):
+
+class Aspect(models.Model):
     content = models.TextField()
 
     def __str__(self):
         return self.content
 
-class Issue (Aspect):
+
+class Issue(Aspect):
     game = models.ForeignKey(Game)
 
-class Face (Aspect):
+
+class Face(Aspect):
     name = models.CharField(max_length=128)
     game = models.ForeignKey(Game)
 
     def __str__(self):
         return self.name
 
-class Place (Aspect):
+
+class Place(Aspect):
     name = models.CharField(max_length=128)
     game = models.ForeignKey(Game)
 
     def __str__(self):
         return self.name
 
-class Skill (models.Model):
+
+class Skill(models.Model):
     name = models.CharField(max_length=128)
     game = models.ForeignKey(Game)
 
