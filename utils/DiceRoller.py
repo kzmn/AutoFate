952a3c4871df936054003a22b865a67f7f4eca72
kzmn <kzmn@vt.edu>
2013-12-25 21:16:32 -0600

    Actually got dice roller sort of working for polyhedral dice

--- utils/DiceRoller.py
+++ utils/DiceRoller.py
@@ -5,16 +5,18 @@ class DiceRoller():
 
 
     @staticmethod
-    def rolldice(**dicedict):
+    def rolldice(dicedict):
         """
         Rolls a set of polyhedral dice and returns a list of the results
-            :param dicedict:  A diction of dice, the key is the number of sides to the
-                die, the value is the number of dice of that type to roll.
+        :param self: the self parameter
+        :param dicedict:  A diction of dice, the key is the number of
+        sides to the die, the value is the number of dice of that type to roll.
         :rtype : (int)
         """
         random.seed()
-        results = [None]
+        results = []
         for key in dicedict.keys():
+            print(key)
             [results.append(random.randrange(1, key, 1)) for _ in
              range(dicedict[key])]
         return results
