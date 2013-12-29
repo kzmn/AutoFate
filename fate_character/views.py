bf5adf62938583b8b85a59781de110f808385dc9
bsc52001 <bsc52001@gmail.com>
2013-12-28 21:04:30 -0600

    Changes to the api data model

--- fate_character/views.py
+++ fate_character/views.py
@@ -1,5 +1,14 @@
 from rest_framework import viewsets
 from fate_character.models import *
 
+
 class CharacterViewSet(viewsets.ModelViewSet):
     model = Character
+
+
+class PlayerCharacterViewSet(viewsets.ModelViewSet):
+    model = PlayerCharacter
+
+
+class FaceOrPlaceCharacterViewSet(viewsets.ModelViewSet):
+    model = FaceOrPlaceCharacter
--- fate_game/views.py
+++ fate_game/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from fate_game.models import *
+from fate_game.serializer import GameSerializer
 
 
 class GameViewSet(viewsets.ModelViewSet):
     model = Game
+    serializer_class = GameSerializer
